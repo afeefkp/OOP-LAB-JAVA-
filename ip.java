9ed5cce476c6ced7922b8a22794f82ab528c60dd
afeefkp <93336792+afeefkp@users.noreply.github.com>
2022-03-17 16:10:33 +0530

    Add files via upload

---
+++ ip.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ip {
+
+	public static void main(String[] args) {
+		Scanner q=new Scanner(System.in);
+		System.out.println("Enter name: ");
+		String name=q.nextLine();
+		System.out.println("Enter Roll No: ");
+		int a=q.nextInt();
+		System.out.println("Enter marks: ");
+		int b=q.nextInt();
+		
+		System.out.println("Name: "+name);
+		System.out.println("Roll No: "+a);
+		System.out.println("Marks obtained: "+b);
+		
+	}
+
+}
